b16348dec91b6febbb0a1360b6a92eb1c7177588
salmaKishk <salma.mohamed2255@gmail.com>
2023-06-03 11:13:33 +0300

    new repo

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def multipl_xy(value_1, value_2):
+    """Sum two numbers"""
+    return value_1 + value_2
+
+
+if __name__ == "__main__":
+    print(multipl_xy(1, 2))
---
+++ test.py
@@ -0,0 +1,7 @@
+from main import multipl_xy
+
+def test_multipl_xy():
+    assert 3 == multipl_xy(1, 3)
+    assert 6 == multipl_xy(2, 3)
+    assert 8 == multipl_xy(2, 4)
+    
